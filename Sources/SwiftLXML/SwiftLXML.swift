38a5247bd40da6b4e4700f9afd123f6b2200f77b
Roderick Lizardo <rodericklizardo@mbp14.local>
2023-12-20 12:48:28 -0800

    Add readXMLDocument function.

--- Sources/SwiftLXML/SwiftLXML.swift
+++ Sources/SwiftLXML/SwiftLXML.swift
@@ -7,3 +7,18 @@
 
 import Foundation
 
+struct SwiftLXML {
+    
+    func ReadXMLDocument(filePath: String) throws -> Any? {
+        do {
+            let xmlData = try Data(contentsOf: URL(fileURLWithPath: filePath))
+            
+            return try XMLDocument(data: xmlData, options: [.nodePrettyPrint,.nodePreserveWhitespace])
+            
+        } catch {
+            print("Error reading XML Document: \(error)")
+            
+            return nil
+        }
+    }
+}
